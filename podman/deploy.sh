1a4b39efafe1584489faf176684e3e3b4e754ec8
Fabio Martinelli <fabio.martinelli@accenture.com>
2023-09-06 15:01:15 +0200

    1st commit

--- podman/deploy.sh
+++ podman/deploy.sh
@@ -1,1 +1,5 @@
+podman images
 podman build -t ubi8-felinks  .
+podman images
+podman run -ti --rm -d --name ubi8-felinks localhost/ubi8-felinks:latest
+podman exec -ti ubi8-felinks /tmp/git/github/felinks/src/elinks www.google.com
