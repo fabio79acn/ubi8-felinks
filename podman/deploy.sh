877bd407aaab43d8a12e9481897c255de374b060
Fabio Martinelli <fabio.martinelli@accenture.com>
2023-09-06 15:11:31 +0200

    2nd version

--- podman/deploy.sh
+++ podman/deploy.sh
@@ -1,5 +1,8 @@
+#!/bin/bash -x
+
+set -euo pipefail
 podman images
-podman build -t ubi8-felinks  .
+podman build -t ubi8-minimal-felinks  ../container-img
 podman images
-podman run -ti --rm -d --name ubi8-felinks localhost/ubi8-felinks:latest
-podman exec -ti ubi8-felinks /tmp/git/github/felinks/src/elinks www.google.com
+podman run -ti --rm -d --name ubi8-minimal-felinks localhost/ubi8-minimal-felinks:latest
+podman exec -ti ubi8-minimal-felinks /tmp/git/github/felinks/src/elinks www.google.com
