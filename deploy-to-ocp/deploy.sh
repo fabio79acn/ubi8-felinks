f80147d3f6c93ca65ade46522e998e58fbd1b5e2
Fabio Martinelli <fabio.martinelli@accenture.com>
2023-09-06 14:30:18 +0200

    1st commit

--- deploy-to-ocp/deploy.sh
+++ deploy-to-ocp/deploy.sh
@@ -5,3 +5,4 @@ oc new-app  --name felinks-1 --strategy docker --context-dir=container-img  http
 ### WARNING ##
 ## if the previous command does not work, git clone locally the git repo on your Linux management host and then:
 # oc new-app  --name felinks-1 --strategy docker --context-dir=/container-img .
+## you local dir must to be the root of the Git repo
