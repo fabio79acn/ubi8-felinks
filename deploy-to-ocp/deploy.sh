7126ac3f1c003c342c52cc89c2efe504ede76337
Fabio Martinelli <fabio.martinelli@accenture.com>
2023-09-06 14:26:50 +0200

    1st commit

--- deploy-to-ocp/deploy.sh
+++ deploy-to-ocp/deploy.sh
@@ -1,2 +1,7 @@
-# oc project YOUR_PROJECT
-oc new-app  --name felinks-1 --context-dir=container-img --source-strategy=docker  https://github.com/fabio79acn/ubi8-felinks
+## oc project YOUR_PROJECT
+oc new-app  --name felinks-1 --strategy docker --context-dir=container-img  https://github.com/fabio79acn/ubi8-felinks
+
+
+### WARNING ##
+## if the previous command does not work, git clone locally the git repo on your Linux management host and then:
+# oc new-app  --name felinks-1 --strategy docker --context-dir=/container-img .
