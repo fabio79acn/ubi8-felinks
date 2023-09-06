44d64dcc72f11fb5263cd2a0b6bff6c403df9464
Fabio Martinelli <fabio.martinelli@accenture.com>
2023-09-06 13:40:58 +0200

    1st commit

--- deploy-to-ocp/deploy.sh
+++ deploy-to-ocp/deploy.sh
@@ -1,2 +1,2 @@
 # oc project YOUR_PROJECT
-oc new-app  --name felinks-1 --context-dir='./container-img'  https://github.com/fabio79acn/ubi8-felinks
+oc new-app  --name felinks-1 --context-dir=container-img --source-strategy=docker  https://github.com/fabio79acn/ubi8-felinks
